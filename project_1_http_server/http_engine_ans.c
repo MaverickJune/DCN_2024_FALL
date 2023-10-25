be89576124a2ea83099aae1b279e78282d80174e
cakeng <cakeng@snu.ac.kr>
2023-10-25 15:50:50 +0900

    POST boundary fixes

--- project_1_http_server/http_engine_ans.c
+++ project_1_http_server/http_engine_ans.c
@@ -205,10 +205,7 @@ http_t *parse_multipart_content_body_ans (char** body_p, char* boundary, size_t 
             break;
         head_ptr++;
     }
-    if (strncmp (head_ptr + strlen(boundary), "--", 2) == 0)
-        head_ptr -= 4; // \r\n--
-    else
-        head_ptr -= 2; // \r\n
+    head_ptr -= 4; // \r\n--
 
     size_t data_size = head_ptr - eoh_ptr;
     if (add_body_to_http (http, data_size, eoh_ptr) == -1)
